48b02b4bba70aab1f722642c8c09ccf1b7b5d889
RickyVo <vytruongvo@gmail.com>
2022-11-05 17:34:34 +0700

    update

--- SchoolX/Sprint_2/assets/js/gridColor.js
+++ SchoolX/Sprint_2/assets/js/gridColor.js
@@ -1,13 +1,5 @@
-const tables = document.querySelector('dataTable');
-const clicker = document.querySelector('bt');
-const arrColor = [
-    ' style="background-color: red" ',
-    ' style="background-color: blue" ',
-    ' style="background-color: yellow" ',
-    ' style="background-color: pink" ',
-    ' style="background-color: violet" '
-]
-clicker.addEventListener("click",function(){
-    const color =arrColor[Math.floor(Math.random()*arrColor.length)];
-    tables.innerHTML = color;
-});
+function color(z) {
+    var a = getComputedStyle(z);
+    var b = a.backgroundColor;
+    document.getElementsByTagName("table")[0].style.backgroundColor=b;
+}
--- SchoolX/Sprint_2/assets/js/ranQuote.js
+++ SchoolX/Sprint_2/assets/js/ranQuote.js
@@ -1,38 +1,30 @@
-arrQuote = [
-    {
-        author: 'Dalai Lama',
-        quote: '"The purpose of our lives is to be happy."',
-    },
-    {
-        author: 'Thomas A. Edison',
-        quote: '"Many of life’s failures are people who did not realize how close they were to success when they gave up."',
-    },
-    {
-        author: 'Babe Ruth',
-        quote: '"Never let the fear of striking out keep you from playing the game."',
-    },
-    {
-        author: 'Will Smith',
-        quote: '"Money and success don’t change people; they merely amplify what is already there."',
-    },
-    {
-        author: 'Steve Jobs',
-        quote: '"Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking."',
-    },
-    {
-        author: 'Seneca',
-        quote: '"Not how long, but how well you have lived is the main thing',
-    },
-    {
-        author: 'Frank Sinatra',
-        quote: '"The big lesson in life, baby, is never be scared of anyone or anything."',
+arrText = [
+        {
+            author: 'Huấn Rose',
+            quote: '"Không làm mà muốn có ăn thì chỉ có ăn đầu b*** ăn c*c ."',
+        },
+        {
+            author: 'Khá Bảnh',
+            quote: '"Trên đời này không có chuyện đúng sai, chỉ có kẻ mạnh và kẻ yếu, thế thôi."',
+        },
+        {
+            author: 'Tiến Bịp',
+            quote: '" Tài 6 3 mà nó còn ra 1 còn xỉu thì 1 4 mà nó còn ra 6, thế nó mới bịp."',
+        },
+        {
+            author: 'Trương Tuấn Tú',
+            quote: '"Người không chơi là người không bao giờ thắng, Người thua là người chưa  thắng, Người bỏ cuộc là thất bại, Còn chơi là còn gỡ  ."',
+        }
+        
+    ];
+    const footer = document.querySelector('.content-bottom')
+    const quotes = document.querySelector('.bottom-textQuote')
+    const signature = document.querySelector('.bottom-textSignature')
+    footer.addEventListener("click",ranText);
+    function ranText(){
+        const item = arrText[Math.floor(Math.random()*arrText.length)];
+        quotes.innerHTML = item.quote;
+        signature.innerHTML = item.author;
+        
     }
-]
-const text = document.querySelector('.bottom-text')
-const quotes = document.querySelector('.bottom-textQuote')
-const signature = document.querySelector('.bottom-textSignature')
-text.addEventListener("click",function() {
-    const item = arrQuote[Math.floor(Math.random()*arrQuote.length)];
-    quotes.innerHTML = item.quote;
-    signature.innerHTML = item.author;
-})
+    console.log(footer);
